09f14149d2659551285f1fc19988be5a66a1a826
tanz <tanz@rose-hulman.edu>
2018-11-26 14:47:53 -0500

    DONE

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -110,7 +110,7 @@ print(type('math'))
 
 ###############################################################################
 #
-# DONE: 6.
+# Done: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
